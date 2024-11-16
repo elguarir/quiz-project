4de708cfcb3274688b501587fafa8b50071a4b97
Mohamed Elguarir <mohaelguarir@gmail.com>
2024-11-16 13:19:36 +0100

    bootstrap project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.quizproject.quizproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.quizproject.quizproject to javafx.fxml;
+    exports org.quizproject.quizproject;
+}
